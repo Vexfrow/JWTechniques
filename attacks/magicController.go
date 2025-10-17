a76e4131cd272f73c156dec346d799a5e241c6d6
Thomas GREGOIRE <thomas.gregoire82@gmail.com>
2025-10-17 11:38:36 +0200

    Magic - Print new token if no error

--- attacks/magicController.go
+++ attacks/magicController.go
@@ -34,7 +34,9 @@ func MainMagic(jwtStr string, userHeader string, userValue string) {
 	if _, ok := token.Header["alg"]; ok {
 		fmt.Print("Checking if the token is vulnerable to the \"none\" algorithm vuln\n\n")
 		newJWTStr := ExploitNoneAlgo(token, userHeader, userValue)
-		fmt.Printf("None algorithm : %s\n\n", newJWTStr)
+		if newJWTStr != "" {
+			fmt.Printf("None algorithm : %s\n\n", newJWTStr)
+		}
 		fmt.Print("------------------------------------------------------\n\n")
 		fmt.Print("You can use Hashcat with the mode 16500 to verify if the JWT is signed with a weak secret\n\n")
 		fmt.Print("------------------------------------------------------\n\n")
@@ -43,14 +45,18 @@ func MainMagic(jwtStr string, userHeader string, userValue string) {
 	if _, ok := token.Header["jku"]; ok {
 		fmt.Print("Your token contains the \"JKU\" header, it can maybe be exploitable through header injection\n\n")
 		newJWTStr := ExploitJKU(token, userHeader, userValue)
-		fmt.Printf("JKU header injection : %s\n\n", newJWTStr)
+		if newJWTStr != "" {
+			fmt.Printf("JKU header injection  : %s\n\n", newJWTStr)
+		}
 		fmt.Print("------------------------------------------------------\n\n")
 	}
 
 	if _, ok := token.Header["kid"]; ok {
 		fmt.Print("Your token contains the \"KID\" header, it can maybe be exploitable through header injection\n\n")
 		newJWTStr := ExploitKID(token)
-		fmt.Printf("KID header injection : %s\n", newJWTStr)
+		if newJWTStr != "" {
+			fmt.Printf("KID header injection : %s\n", newJWTStr)
+		}
 		fmt.Print("------------------------------------------------------\n\n")
 	}
 
